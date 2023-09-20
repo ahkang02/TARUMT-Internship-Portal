5e0db6f38cd6c869f547e7673e6517928dd58d68
Kang Ah <ahkang02@outlook.com>
2023-09-20 20:34:54 +0800

    Configuration Files

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-app.config['S3_BUCKET'] = "S3_BUCKET_NAME"
-app.config['S3_KEY'] = "AWS_ACCESS_KEY"
-app.config['S3_SECRET'] = "AWS_ACCESS_SECRET"
-app.config['S3_LOCATION'] = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+S3_BUCKET_NAME = "s3-bucket-name"
+RDS_HOST_ENDPOINT = "rds-host-endpoint"
+RDS_LOGIN = "rds-login"
+RDS_PASS = "rds-pass"
+AWS_REGION = "aws-region"
+DB_INSTANCE_NAME = "db-instance-name"
