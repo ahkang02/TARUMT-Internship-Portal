958240545e9142cb9c460eac3eada0a60b3cd1d5
Kang Ah <ahkang02@outlook.com>
2023-09-21 00:06:04 +0800

    Login & Register Settle

--- app.py
+++ app.py
@@ -5,21 +5,29 @@ from config import *
 app = Flask(__name__)
 
 # Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user = RDS_LOGIN,
-        password = RDS_PASS,
-        host = RDS_HOST_ENDPOINT,
-        port = DB_PORT,
-        database = DB_INSTANCE_NAME
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+# try:
+#    conn = mariadb.connect(
+#        user = RDS_LOGIN,
+#        password = RDS_PASS,
+#        host = RDS_HOST_ENDPOINT,
+#        port = DB_PORT,
+#        database = DB_INSTANCE_NAME
+#    )
+# except mariadb.Error as e:
+#    print(f"Error connecting to MariaDB Platform: {e}")
+#    sys.exit(1)
     
 # Get Cursor
-cur = conn.cursor()
+# cur = conn.cursor()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('sign-up.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
