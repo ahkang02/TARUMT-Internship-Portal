4de41bb02805101854c7082d80b3287241f213bd
Kang Ah <ahkang02@outlook.com>
2023-09-22 17:47:36 +0800

    Login, Header, Session Done

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import mariadb, sys
 from config import *
+import uuid
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 # Connect to MariaDB Platform
 # try:
@@ -20,18 +22,63 @@ app = Flask(__name__)
 # Get Cursor
 # cur = conn.cursor()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+
+    if request.method == 'POST':
+        level = request.form['level']
+        cohort = request.form['cohort']
+        programme = request.form['programme']
+        tutGrp = request.form['tutorialGrp']
+        studID = request.form['studentID']
+        studEmail = request.form["studentEmail"]
+        studentSupervisor = request.form["ucSupervisor"]
+        studentSuperEmail = request.form["ucSupervisorEmail"]
+        studFullName = request.form["studentFullName"]
+        studIC = request.form["studIC"]
+        studGender = request.form["studGender"]
+        studTransport = request.form["studTransport"]
+        studHealthRemark = request.form["studHealthRemark"]
+        studPersonalEmail = request.form["studPersonalEmail"]
+        studTermAddress = request.form["studTermAddress"]
+        studPermanentAddress = request.form["studPermanentAddress"]
+        studMobileNumber = request.form["studMobileNumber"]
+        studFixedNumber = request.form["studFixedNumber"]
+        studTechnicalSkills = request.form["studTechnicalSkills"]
+        studDatabaseSkills = request.form["studDatabaseSkills"]
+        studNetworkingSkills = request.form["studNetworkingSkills"]
+
+        insert_sql = "INSERT INTO STUDENTS VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)"
+        return redirect(url_for('login'))
+
     return render_template('sign-up.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        session["username"] = username
+
+        return redirect(url_for('profile'))
+    else:
+        if "user" in session:
+            return redirect(url_for('profile'))
     return render_template('login.html')
+    
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('index'))
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
     return render_template('profile.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+import uuid 
+
 S3_BUCKET_NAME = "s3-bucket-name"
 RDS_HOST_ENDPOINT = "rds-host-endpoint"
 RDS_LOGIN = "rds-login"
 RDS_PASS = "rds-pass"
 AWS_REGION = "aws-region"
 DB_INSTANCE_NAME = "db-instance-name"
-DB_PORT = 3306
+DB_PORT = 3306 
+SECRET_KEY = uuid.uuid4().hex
